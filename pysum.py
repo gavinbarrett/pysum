7af3392722fdb62adc75f26c43f2e5983b03a50a
Gavin Barrett <gavinbrrtt@gmail.com>
2019-03-06 22:30:23 -0800

    refactored: added error checking, authentication fn, more robust hash searching method

--- pysum.py
+++ pysum.py
@@ -1,38 +1,67 @@
 #Created by Gavin Barrett on 08-30-18
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from hashlib import sha256
+from nltk.tokenize import word_tokenize
 import sys
-#read in checksum and iso respectively
-checksum = sys.argv[1]
-isoFile = sys.argv[2]
-#get checksum
-with open(checksum, 'r') as chksm:
-    for line in chksm:
-        value = chksm.readline()
-        if value.find(isoFile) > -1:
-            break
-        else:
-            if(len(value) == 0):
-                value = 0
-chksm.close()
-value2 = value.split(' *')
-value = value2[0]
-#blocksize to read file
-BLOCKSIZE = 4096
-#create hashing obj
-hasher = sha256()
-with open(isoFile, 'rb') as hashand:
-        while True:
-            chunk = hashand.read(BLOCKSIZE)
-            if not chunk:
-                break
-            hasher.update(chunk)
-hash_string = hasher.hexdigest()
-hashand.close()
-print('\n')
-print("Expected checksum: " + value)
-print("Calculated checksum: " + hash_string)
-if(value == hash_string):
-    print("Good checksum match!")
-    print('\n')
+import os
 
+def check_arguments():
+    ''' Check for argument errors '''
+    if  not os.path.isfile(sys.argv[1]) or not os.path.isfile(sys.argv[2]):
+        print('Usage: <checksum.txt> <iso.iso>')
+        sys.exit(0)
+    if not sys.argv[2].endswith('.iso'):
+        print('Usage: <checksum.txt> <iso.iso>')
+        sys.exit(0)
+    return sys.argv[1], sys.argv[2]
+
+def get_hashsum(hashsum, iso):
+    ''' Receive expected checksum value '''
+    with open(hashsum, 'r') as hsum:
+        for line in hsum:
+            if line.find(iso) != -1:
+                # extract hash
+                if line.find('SHA256') != -1 or line.find('sha256') != -1:
+                    tokens = word_tokenize(line)
+                    for token in tokens:
+                        if len(token) == 64:
+                            return token
+        print('Hash retrieval failed..')
+        sys.exit(0)
+
+def hash_file(isoFile):
+    ''' Hash the .iso file with sha256 '''
+    #blocksize to read file
+    BLOCKSIZE = 4096
+    #create hashing obj
+    hasher = sha256()
+    with open(isoFile, 'rb') as f:
+            while True:
+                chunk = f.read(BLOCKSIZE)
+                if not chunk:
+                    break
+                hasher.update(chunk)
+    return hasher.hexdigest()
+
+def authenticated(value, hash_string):
+    ''' Authenticate .iso '''
+    print('\n')
+    print('Expected hash: ' + value)
+    print('Calculated hash: ' + hash_string)
+    return value == hash_string
+
+def main():
+    # check files for basic errors
+    hashsum, iso = check_arguments()
+    # obtain expected hash value
+    value = get_hashsum(hashsum, iso)
+    # hash the .iso file
+    hash_string = hash_file(iso)
+    
+    if authenticated(value, hash_string):
+        print('Good checksum match!\n')
+    else:
+        print('Hash values do not match!\n')
+
+if __name__ == "__main__":
+    main()
